03720e85976f735375230ec8f417c761febc1364
Manabu Matsuzaki <matsumana@gmail.com>
2015-01-23 02:01:34 +0900

    アプリのエントリポイントにこのアノテーションは不要です

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,10 +3,7 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @ComponentScan
 @EnableAutoConfiguration
 public class App {
